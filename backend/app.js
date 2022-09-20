b0bcf6c33d3e66f5183593acc0e1f872b096f2d9
Jlndl <jaakko.lundell@gmail.com>
2022-09-20 10:04:39 +0300

    Päivitetty app.js, korjattu pieni virhe

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 const {
-createRecipes
-readRecipes
-updateRecipes
-deleteRecipes
+  createRecipes,
+  readRecipes,
+  updateRecipes,
+  deleteRecipes,
 } = require("./database");
 
 app.use(cors());
 app.use(express.json());
 
 //---
-app.listen(port, () =>{
-console.log(`App listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 });
