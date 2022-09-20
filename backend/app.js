1fa68dccea19938546224d306def187a99bb79bb
Jlndl <jaakko.lundell@gmail.com>
2022-09-20 11:40:37 +0300

    get ja post pyyntöjen testailua

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,19 @@ const {
 app.use(cors());
 app.use(express.json());
 
+app.get("/recipes", async (req, res) => {
+  res.json(await readRecipes());
+});
+
+app.get("/recipes/:id", async (req, res) => {
+  const id = req.params.id;
+  res.json(await readRecipe(id));
+});
+
+app.post("/recipes", async (req, res) => {
+  res.json(await createRecipes(req.body));
+});
+
 //---
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
