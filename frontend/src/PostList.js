6415febdaeda2b48d2fcf36962a257f494ba70eb
Samu Landen <samu.landen@gmail.com>
2022-09-21 09:07:13 +0300

    made creating recipe work

--- frontend/src/PostList.js
+++ frontend/src/PostList.js
@@ -85,7 +85,7 @@ const persistDeleteRequest = async (apiUrl, id) => {
 };
 
 const persistRecipe = async (apiUrl, recipe) => {
-  await fetch(apiUrl + "/recipe", {
+  await fetch(apiUrl, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(recipe),
